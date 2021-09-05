5dfaa83bb687f8abc8922cbdd3cb172a7a59d4a3
Wuletaw Wonte <wuletawwonte@gmail.com>
2021-09-05 22:56:44 +0300

    Error Handling done for unknown endpoints

--- app.js
+++ app.js
@@ -9,5 +9,19 @@ app.use(express.json());
 
 app.use('/user', userRoute);
 
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+})
 
 module.exports = app;
