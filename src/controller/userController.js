fd99db58892148b5e67c5eae9e90c15bd66a6d6a
Wuletaw Wonte <wuletawwonte@gmail.com>
2021-09-08 17:04:53 +0300

    duplicate user error handling done

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -33,32 +33,48 @@ exports.getUser = (req, res) => {
     });
 };
 
-exports.registerUser = (req, res) => {
-  bcrypt.hash(req.body.password, 10, (err, hash) => {
-    if (err) {
-      return res.status(500).json({
-        message: err,
-      });
-    } else {
-      const user = new User({
-        _id: mongoose.Types.ObjectId(),
-        username: req.body.username,
-        password: hash,
-      });
-
-      user
-        .save()
-        .then(() => {
-          res.status(200).json({
-            message: "User added successfully",
-            data: user,
-          });
-        })
-        .catch((err) => {
-          res.status(500).json({
-            message: "Internal Server Error: " + err
-          })
+exports.registerUser = (req, res, next) => {
+  User.find({ username: req.body.username })
+    .then((user) => {
+      if (user.length >= 1) {
+        console.log("User Already Exists");
+        res.status(409).json({
+          message: "User already exists"
         });
-    }
-  });
+      } else {
+        bcrypt.hash(req.body.password, 10, (err, hash) => {
+          if (err) {
+            return res.status(500).json({
+              message: err,
+            });
+          } else {
+            const user = new User({
+              _id: mongoose.Types.ObjectId(),
+              username: req.body.username,
+              password: hash,
+            });
+
+            user
+              .save()
+              .then(() => {
+                res.status(200).json({
+                  message: "User added successfully",
+                  data: user,
+                });
+              })
+              .catch((err) => {
+                res.status(500).json({
+                  message: "Internal Server Error: " + err,
+                });
+              });
+          }
+        });
+      }
+    })
+    .catch((err) => {
+      console.log("Internal Server Error: " + err);
+      res.status(500).json({
+        message: "Internal Server Error: " + err,
+      });
+    });
 };
