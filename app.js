da736df8cc6690a1b9dcca5c01a06f8323e3a8f1
Wuletaw Wonte <wuletawwonte@gmail.com>
2021-09-08 16:55:16 +0300

    api folder renamed to src

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const mongoose = require('mongoose');
 
 // import routes
-const userRoutes = require('./api/routes/user');
+const userRoutes = require('./src/routes/user');
 
 const connectionString = "mongodb://192.168.56.104:27017/fetnocamp";
 // const localConnectionString = "mongodb+srv://wuletaw:" + process.env.MONGO_ATLAS_PWD + "@fetnocamp.jzx9n.mongodb.net/fetnocamp?retryWrites=true&w=majority";
